1cd1980d640b0faa462a7755b6388a64a2f0cc3a
Mehedi Hassan Sazol <mhsazol111@gmail.com>
2021-07-08 18:44:31 +0600

    fixing localStorage issue

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ const ulLIst = document.querySelector(".list-item");
 const form = document.querySelector(".input-section")
 const inputV = document.querySelector(".input1");
 const todoBtn = document.querySelector(".btn1");
+const exitingData = JSON.parse(localStorage.getItem("todos"));
 
-const todos = JSON.parse(localStorage.getItem("todos"));
+// Render Existing Lists
+if(exitingData) {
+  exitingData.map((item) => {
+    addTodo(item);
+  })
+}
 
-todoBtn.addEventListener("click", addTodo);
-
-function addTodo(event) {
+// Add todo on the list and save to localStorage
+todoBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  let todoText = inputV.value;
-  if (todoText == "" || inputV.value.trim() == "") {
+  addTodo();
+});
+
+
+function addTodo(existingValue) {
+  const todoText = existingValue || inputV.value;
+
+  if (todoText.trim() == "") {
     alert("type something");
   } else {
     // create DIV
@@ -28,52 +39,57 @@ function addTodo(event) {
     checkBtn.innerHTML = '<i class="fas fa-check"></i>';
     todo.appendChild(checkBtn);
 
-    // Event Listener
+    // trash btn
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("trash");
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    todo.appendChild(deleteBtn);
 
+    // Event Listener
     checkBtn.addEventListener("click", function (x) {
       const mark = x.target;
       if (mark.classList[0] === "check-btn") {
         const markEl = mark.parentElement;
         markEl.classList.toggle("checked");
       }
-      store();
+      const todoWrapper = mark.parentElement;
+      const data = todoWrapper.querySelector('li');
+      store(data.innerHTML);
     });
-    // trash btn
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("trash");
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    todo.appendChild(deleteBtn);
-
+  
      // Event Listener
-
     deleteBtn.addEventListener("click", function (e) {
       const item = e.target;
       if (item.classList[0] === "trash") {
         const todoItem = item.parentElement;
         todoItem.classList.add("fall");
-        todoItem.addEventListener("transitionend", function () {
-          todoItem.remove();
-        });
+        const todoWrapper = item.parentElement;
+          const data = todoWrapper.querySelector('li');
+        todoItem.addEventListener("transitionend", remove(data.innerHTML));
+        ;
       }
     });
-
-    // all clear btn
-
-
   }
   inputV.value = "";
 }
 
-function store() {
-  const todosEl = document.querySelectorAll("li");
-
-  const todos = [];
-  todosEl.forEach((todo) => {
-      todos.push(todo.value);
-      console.log(todo);
-  });
+// Store Data on localStorage
+function store(data) {
+  const todos = JSON.parse( localStorage.getItem('todos') ) || [];
+  if(todos.includes(data)) {
+    return false;
+  }
+  todos.push(data);
 
   localStorage.setItem("todos", JSON.stringify(todos));
-  
+}
+
+// Remove Data from localStorage
+function remove(data) {
+  const exitingData = JSON.parse(localStorage.getItem('todos'));
+  const todos = exitingData.filter( item => item !== data);
+
+  if(todos) {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
 }
